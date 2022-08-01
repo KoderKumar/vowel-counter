df599dc8c7f1b33c567071dbde23d9fb763ea219
ArthKumar <48092253+KoderKumar@users.noreply.github.com>
2022-08-01 07:52:15 +0530

    Add files via upload

---
+++ Vowel-Counter.py
@@ -0,0 +1,12 @@
+def countVowels(sentence):
+	count = 0
+	sentence = sentence.lower()
+	for c in sentence:
+		if c in ['a', 'e', 'i', 'o', 'u']:
+			count += 1
+	return count
+
+if __name__ == '__main__':
+	userInput = str(input("Enter the string to check: "))
+	count  = countVowels(userInput)
+	print('Vowel Count: ', count)
